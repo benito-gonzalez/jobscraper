2ac951dc21c999199c64d384b0441da763f60acb
Benito Gonzalez <gon.beni@gmail.com>
2019-01-02 18:33:11 +0100

    Media files are redirected only in Production mode

--- job_scraper/urls.py
+++ job_scraper/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.views.generic.base import RedirectView
+from django.conf import settings
 from django.conf.urls import url
 from . import views
 
@@ -7,5 +8,7 @@ app_name = 'job_scraper'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    url(r'^.*$', RedirectView.as_view(url='/', permanent=False), name='index')
 ]
+
+if not settings.DEBUG:
+    urlpatterns.append(url(r'^.*$', RedirectView.as_view(url='/', permanent=False), name='index'))
